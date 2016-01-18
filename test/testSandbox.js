ba943d455c58076eefacbf251e956870876485c3
Christian Schuller <cschuller@servusalps.com>
2016-01-18 22:44:51 +0100

    Added documentation.

--- test/testSandbox.js
+++ test/testSandbox.js
@@ -1,15 +1,15 @@
 /* global: global */
 
+// Setup of test libs
 global.chai = require('chai');
 global.sinon = require('sinon');
 global.sinonChai = require('sinon-chai');
 global.expect = global.chai.expect;
 global.chai.use(global.sinonChai);
 
+// Setting global dependencies
 global.yak = {};
-
 global._ = require('underscore');
-
 
 // Replace the global logger with a quit one.
 yak.Logger = require('./loggerStub').LoggerStub;
