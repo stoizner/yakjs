c9e302350664332717b9bebeff8b87322c5a08a6
Christian Schuller <cschuller@servusalps.com>
2017-07-26 17:53:50 +0200

    Adds new API to get log files.

--- server/src/routes/v1/apiV1Router.js
+++ server/src/routes/v1/apiV1Router.js
@@ -5,13 +5,14 @@ const router = express.Router(); // eslint-disable-line new-cap
 const cors = require('cors');
 
 router.use(cors());
-router.use(require('./instances/router'));
-router.use(require('./plugins/router'));
-router.use(require('./storeItems/router'));
-router.use(require('./modules/router'));
-router.use(require('./upload/router'));
-router.use(require('./package/router'));
 router.use(require('./commands/router'));
 router.use(require('./commands/presets/router'));
+router.use(require('./instances/router'));
+router.use(require('./logs/router'));
+router.use(require('./modules/router'));
+router.use(require('./package/router'));
+router.use(require('./plugins/router'));
+router.use(require('./storeItems/router'));
+router.use(require('./upload/router'));
 
 module.exports = router;
