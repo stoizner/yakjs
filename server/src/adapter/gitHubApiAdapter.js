9da79399b6aaaacb887b877acd6368b0fc858b4d
Christian Schuller <cschuller@servusalps.com>
2018-02-10 09:54:00 +0100

    Adds git hub API adapter.

---
+++ server/src/adapter/gitHubApiAdapter.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const https = require('https');
+
+const userAgent = 'YAKjs';
+
+/**
+ * @class
+ */
+class GutHubApiAdapter {
+    /**
+     * @param {string} path
+     * @returns {!Promise<Object>}
+     */
+    get(path) {
+        const gitHubOptions = {
+            protocol: 'https:',
+            hostname: 'api.github.com',
+            port: 443,
+            path: path,
+            contentType: 'application/json',
+            dataType: 'text',
+            timeout: 2000,
+            headers: {
+                'User-Agent': userAgent
+            }
+        };
+
+        return new Promise((resolve, reject) => {
+            https.get(gitHubOptions, response => {
+                let data = '';
+                response.on('data', chunk => {
+                    data += chunk;
+                });
+                response.on('end', () => resolve(JSON.parse(data)));
+            }).on('error', reject);
+        });
+    }
+}
+
+module.exports = new GutHubApiAdapter();
