68c2dd8e3a24f92907f65be7ec660107087d6fb0
Christian Schuller <cschuller@servusalps.com>
2018-04-01 17:26:55 +0200

    Adds a plugin command to send a "ping".

--- server/plugins/pingPong.plugin.js
+++ server/plugins/pingPong.plugin.js
@@ -19,9 +19,35 @@ function PingPongPlugin() {
     };
 }
 
+/**
+ * @param {?} data
+ * @param {PluginContext} context
+ * @param {CommandConfig} command
+ */
+function executeSendPingCommand(data, context, command) {
+    let connections = context.instance.getConnections();
+
+    context.log.debug('Execute command: ' + command.name);
+    connections.forEach(connection => {
+        context.log.debug('Sending ping to ' + connection.id);
+        connection.send('ping');
+    });
+
+    return Promise.resolve();
+}
+
 module.exports = {
     name: 'pingPong',
     description: 'For every received "ping" string, this plugin will send an "pong" string.',
-    createWorker: () => new PingPongPlugin()
+    createWorker: () => new PingPongPlugin(),
+    commands: [
+        {
+            name: 'sendPing',
+            displayName: 'Send a ping',
+            description: 'Send a ping message to all connected clients',
+            execute: executeSendPingCommand,
+            exampleData: {}
+        }
+    ]
 };
 
