7a6c6754fe7dd3e98abee3d991d1c1b70b503a58
Christian Schuller <cschuller@servusalps.com>
2020-06-30 08:38:08 +0200

    Add utility to create base64 data uris

---
+++ gulp/createIconDataUri.js
@@ -0,0 +1,10 @@
+const iconPath = require('@mdi/js').mdiChevronRight;
+
+const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" version="1.1"><path fill="#606060" d="${iconPath}" /></svg>`;
+const buffer = Buffer.from(svg);
+const base64Svg = buffer.toString('base64');
+
+const dataUri = `data:image/svg+xml;base64,${base64Svg}`;
+
+console.log(iconPath);
+console.log(dataUri);
